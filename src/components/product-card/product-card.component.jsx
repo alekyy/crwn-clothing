e604deed6440929668f7b80c2011599cb8a35cac
Guilherme Nunes <guilherme.nunes@dataart.com>
2023-09-06 13:42:12 -0300

    Created cart icon and dropdown together with the cart context

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -13,7 +18,9 @@ const ProductCard = ({ product }) => {
         <span className="price">{price}</span>
       </div>
 
-      <Button buttonType="inverted">ADD TO CARD</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        ADD TO CARD
+      </Button>
     </div>
   );
 };
